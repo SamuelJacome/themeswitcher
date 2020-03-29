022c6e68d10e05719042ade39ceeae13a7f5633b
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-29 01:06:55 -0300

    Adicionando switch

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {Container} from './styles'
+import {Container} from './styles';
+import Switch from 'react-switch';
+
 
 const Header: React.FC = () => {
     return (
         <Container>
             Hello World
+
+            <Switch 
+                onChange = {() => {}}
+                checked = {true}
+                checkedIcon = {false}
+                uncheckedIcon = {false}
+            />
+                
         </Container>
 
     );
