4cfc947dcb1ea39a6c013a2500c8ce8b5ea3c7de
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-29 00:52:03 -0300

    Criando o header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import light from './styles/themes/light';
+
+import GlobalStyle from './styles/global'
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-       
+      <ThemeProvider theme={light}>
+        <GlobalStyle />
+        <Header />
+      </ThemeProvider>
+
     </div>
   );
 }
