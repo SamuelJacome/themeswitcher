5e8e814e2f3b6436d62664521c5e422858db819e
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-29 01:13:58 -0300

    att

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {Container} from './styles';
 import Switch from 'react-switch';
+import {ThemeContext} from 'styled-components';
+import {shade} from 'polished';
 
 
 const Header: React.FC = () => {
+
+    const {colors} = useContext(ThemeContext);
+
     return (
         <Container>
             Hello World
@@ -13,6 +18,11 @@ const Header: React.FC = () => {
                 checked = {true}
                 checkedIcon = {false}
                 uncheckedIcon = {false}
+                height= {10}
+                width = {40}
+                handleDiameter = {20}
+                offColor = {shade(0.15, colors.primary)}
+                onColor = {colors.secundary}
             />
                 
         </Container>
