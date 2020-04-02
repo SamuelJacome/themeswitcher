7a4ad9ff931c1644180297e38e01fe6813d1b8fd
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-04-02 16:48:06 -0300

    tema dark ok

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import light from './styles/themes/light';
-import dark from './styles/themes/light'
+import dark from './styles/themes/dark';
 
 import GlobalStyle from './styles/global'
 import Header from './components/Header';
